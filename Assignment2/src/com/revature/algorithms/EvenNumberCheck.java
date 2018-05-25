571c72e596456eaec2edb56f9b864be7020960a1
mfboulos <mboulos95@gmail.com>
2018-05-25 09:01:42 -0400

    Modified answer to #6 to better follow requirements

--- Assignment2/src/com/revature/algorithms/EvenNumberCheck.java
+++ Assignment2/src/com/revature/algorithms/EvenNumberCheck.java
@@ -9,9 +9,10 @@ public class EvenNumberCheck {
 	// checks whether or not a number is even or odd by looking at the result of the number mod 2
 	// prints the result
 	public static void checkIfEven(int n) {
-		int check = n % 2;
+		int intDiv = n / 2;
+		double doubleDiv = n / 2.0;
 		String not = "";
-		if(check == 1)
+		if(intDiv < doubleDiv)
 			not = "NOT ";
 		System.out.println(n + " is " + not + "even.");
 	}
