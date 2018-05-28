067c8f1e3a56647a9fe06fb350a8a3aae0469721
Sydney Mercier <sydney.mercier91@gmail.com>
2018-05-27 20:22:53 -0400

    substrings problem

---
+++ CoreJava/src/Substrings.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Substrings {
+	
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		// get the string and index values from user
+		System.out.println("Enter a string: ");
+		String str = sc.next();
+		System.out.println("Enter a substring length: ");
+		int idx = sc.nextInt();
+		
+		System.out.println(subString(str, idx));
+	}
+	
+	public static String subString(String str, int idx) {
+		String newStr = ""; // create new string for substring
+		
+		for (int i=0; i<idx; i++) {
+			newStr = newStr + str.charAt(i); // append to new string
+		}
+		
+		return newStr;
+	}
+
+}
