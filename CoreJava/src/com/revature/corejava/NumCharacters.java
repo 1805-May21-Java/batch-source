44278e9d13df1387ef2336a4ab7b11e1b2cfec22
Sydney Mercier <sydney.mercier91@gmail.com>
2018-05-28 01:46:14 -0400

    number of characters problem

---
+++ CoreJava/src/com/revature/corejava/NumCharacters.java
@@ -0,0 +1,19 @@
+package com.revature.corejava;
+
+import java.util.Scanner;
+
+public class NumCharacters {
+	
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a string: ");
+		String input = sc.nextLine(); // get string from user
+		
+		// print the number of characters (length of string)
+		System.out.println("Number of characters in string: " + input.length());
+
+	}
+
+}
