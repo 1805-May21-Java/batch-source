9813d98048423740d72a4d9fada6434111b2b757
Sydney Mercier <sydney.mercier91@gmail.com>
2018-05-28 02:05:55 -0400

    simple interest calculator problem

---
+++ CoreJava/src/com/revature/corejava/SimpleInterest.java
@@ -0,0 +1,22 @@
+package com.revature.corejava;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+	
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		// get the principal, rate of interest, and number of years from user:
+		System.out.println("Enter principal amount: ");
+		float principal = sc.nextFloat();
+		System.out.println("Enter the interest rate: ");
+		float interestRate = sc.nextFloat();
+		System.out.println("Enter the number of years (int): ");
+		int numYears = sc.nextInt();
+		
+		System.out.println("Interest is: " + principal * interestRate * numYears);
+	}
+
+}
