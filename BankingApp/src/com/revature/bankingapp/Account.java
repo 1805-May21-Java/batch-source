695d9f03e4fb8622e2f229f78acf15484137f134
Sydney Mercier <sydney.mercier91@gmail.com>
2018-05-29 00:27:49 -0400

    implemented login

---
+++ BankingApp/src/com/revature/bankingapp/Account.java
@@ -0,0 +1,45 @@
+package com.revature.bankingapp;
+
+public class Account {
+	
+	private String username;
+	private String password;
+	private double balance;
+
+	public Account(String username, String password, double balance) {
+		this.username = username;
+		this.password = password;
+		this.balance = balance;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [username=" + username + ", password=" + password + ", balance=" + balance + "]";
+	}
+	
+	
+}
--- BankingApp/src/com/revature/bankingapp/Driver.java
+++ BankingApp/src/com/revature/bankingapp/Driver.java
@@ -1,10 +1,16 @@
 package com.revature.bankingapp;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver {
 	
 	private static Scanner sc = new Scanner(System.in);
+	private static Account session;
+	private static ArrayList<Account> accounts;
 
 	public static void main(String[] args) {
 		
@@ -17,17 +23,38 @@ public class Driver {
 		 * withdraw money
 		 * view balance
 		 */
+		accounts = new ArrayList<Account>();
 		
-		mainPage(); // start bank program	
+		// Read in data file with account info
+		String path = "src/com/revature/bankingapp/data.txt";
+		
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(path));
+			String line = br.readLine();
+			
+			// read data file
+			while (line != null) {
+				String[] arr = line.split(":"); // split lines on colon
+				Account user = new Account(arr[0], arr[1], Double.parseDouble(arr[2]));
+				accounts.add(user);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		mainPage(); // start bank program
+		
 	}
 	
 	// Welcome screen
 	public static void mainPage() {
-		System.out.println("******************************");
-		System.out.println("*                            *");
-		System.out.println("*  Welcome to Bank Mercier!  *");
-		System.out.println("*                            *");
-		System.out.println("******************************");
+		System.out.println("*******************************");
+		System.out.println("*                             *");
+		System.out.println("*  Welcome to Revature Bank!  *");
+		System.out.println("*                             *");
+		System.out.println("*******************************");
 		System.out.println();
 		
 		loginMenu(); // call up navigation
@@ -45,11 +72,9 @@ public class Driver {
 		
 		// Get user input
 		System.out.print("Select an option: ");
-		String input = sc.next().toLowerCase(); // not case sensitive
+		String input = sc.nextLine().toLowerCase(); // not case sensitive
 		System.out.println();
 		if (input.equals("login")) {
-			System.out.println("Logging you in...");
-			System.out.println();
 			loginPage(); // log in
 		} else if (input.equals("new")) {
 			System.out.println("Creating new account...");
@@ -58,7 +83,7 @@ public class Driver {
 		} else if (input.equals("exit")) {
 			System.out.println("Goodbye!"); // quit program
 		} else {
-			System.out.println("Input was not valid. Try again.");
+			System.out.println("ERROR: Input was not valid. Try again.");
 			System.out.println();
 			loginMenu(); // refresh menu when invalid input is entered
 		}
@@ -70,6 +95,40 @@ public class Driver {
 		System.out.println("*  User Login  *");
 		System.out.println("*              *");
 		System.out.println("****************");
+		System.out.println();
+		loginForm();
+	}
+	
+	public static void loginForm() {
+		// get username and password from user
+		System.out.println("All fields are case sensitive.\n");
+		System.out.print("      Enter username (or \"cancel\" to return to main page): ");
+		String username = sc.nextLine(); // case sensitive
+		System.out.println();
+		
+		if (username.equals("cancel")) {
+			mainPage(); // return to main page
+		} else if (listContains(username)){ // check if user exists in system
+			System.out.print("      Enter password (or \"cancel\" to return to main page): ");
+			String password = sc.nextLine(); // case sensitive
+			System.out.println();
+			
+			if (password.equals("cancel")) {
+				mainPage(); // return to main page
+			} else if (password.equals(session.getPassword())) {
+				System.out.println("Logging you in...");
+				System.out.println();
+				dashboardPage(); // log in
+			} else { // password is invalid
+				System.out.println("ERROR: Invalid username/password combo. Try again.");
+				System.out.println();
+				loginForm();
+			}
+		} else {
+			System.out.println("ERROR: User does not exist. Try again.");
+			System.out.println();
+			loginForm();
+		}
 	}
 	
 	public static void newUserPage() {
@@ -78,6 +137,28 @@ public class Driver {
 		System.out.println("*  Create New User  *");
 		System.out.println("*                   *");
 		System.out.println("*********************");
+		System.out.println();
 	}
+	
+	public static void dashboardPage() {
+		System.out.println("***************");
+		System.out.println("*             *");
+		System.out.println("*  Dashboard  *");
+		System.out.println("*             *");
+		System.out.println("***************");
+		System.out.println();
+	}
+	
+	public static boolean listContains(String username) {
+		// iterate thru user accounts
+		for (Account account : accounts) {
+			if (account.getUsername().equals(username)) {
+				session = account; // set current session user
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 }
