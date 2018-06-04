ef0140df78ab1514cd4a0fcc1249913001cbd475
MosesExe <ogoextreme@gmail.com>
2018-06-04 18:50:55 -0400

    Delete BankDrive.java

--- BankDatabase/src/com/revature/banking/BankDrive.java
+++
@@ -1,8 +0,0 @@
-package com.revature.banking;
-import java.sql.*;
-
-public class BankDrive {
-	String host = "keandredb.c7c3jiumkydd.us-east-1.rds.amazonaws.com\n";
-	String uName = "ogoextreme";
-	String pass = "";
-}
