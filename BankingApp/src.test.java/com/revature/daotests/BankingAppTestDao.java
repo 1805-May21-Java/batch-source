c7e575e1f53479f31c44a97e8f534400e9d526d2
Sydney Mercier <sydney.mercier91@gmail.com>
2018-06-05 02:25:19 -0400

    refactoring and JUnit tests

---
+++ BankingApp/src.test.java/com/revature/daotests/BankingAppTestDao.java
@@ -0,0 +1,53 @@
+package com.revature.daotests;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.revature.bankingapp.Account;
+import com.revature.dao.AccountDaoImpl;
+
+public class BankingAppTestDao {
+	
+	private Account account;
+	private List<Account> accounts;
+	private AccountDaoImpl adi = new AccountDaoImpl();
+	
+
+	@Test
+	public void getAccountsTest() {
+		accounts = adi.getAccounts();
+		assertNotNull(accounts);
+	}
+	
+	@Test
+	public void getAccountByIdTest() {
+		Account a = adi.getAccountById(23);
+		assert(a.getUsername().equals("Sydney27"));
+	}
+	
+	@Test
+	public void createAccountTest() {
+		account = new Account("apollo11", "houston", 0.0);
+		adi.createAccount(account);
+		assertNotNull(account.getId());
+	}
+	
+	@Test
+	public void updateAccountTest() {
+		Account a = adi.getAccountById(23);
+		a.setBalance(55);
+		adi.updateAccount(a);
+		assert(a.getBalance() == 55);
+	}
+	
+//	public List<Account> getAccounts();
+//	public Account getAccountById(int id);
+//	public int createAccount(Account account);
+//	public int updateAccount(Account account);
+
+}
