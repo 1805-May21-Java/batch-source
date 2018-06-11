f82b4e39e9220da29fa452ea8aea497436538130
tjansen17 <tnj@17cosmicln.net>
2018-06-11 11:53:54 -0400

    Coding challenge

---
+++ CodingChallenge/page.js
@@ -0,0 +1,105 @@
+// document.getElementById('form-randomize').onclick = rndmuser;
+
+let url = 'https://randomuser.me/api/';
+
+function rndmuser() {
+    sendAjaxGet(url, getrndmuser);
+}
+
+function sendAjaxGet(url, func) {
+    xhr = (new XMLHttpRequest() || new ActiveXObject('Microsoft.HTTPRequest'));
+    xhr.onreadystatechange = function () {
+        if (this.status == 200 && this.readyState == 4) {
+            func(this);
+        }
+    }
+
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+function getrndmuser(xhr) {
+    let resp = xhr.response;
+    let info = JSON.parse(resp).results[0];
+
+    let firstname = info.name.first;
+    let lastname = info.name.last;
+    let email = info.email;
+    let phone = info.phone;
+    let gender = info.gender;
+
+    document.getElementById('firstname').value = firstname;
+    document.getElementById('lastname').value = lastname;
+    document.getElementById('email').value = email;
+    document.getElementById('phone').value = phone;
+
+    let t = document.getElementsByName('gender');
+    for (i of t) {
+        if (i.value == gender) {
+            i.checked = true;
+        } else {
+            i.checked = false;
+        }
+    }
+}
+
+function submittotable(){
+
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+
+    let t = document.getElementsByName('gender');
+    let gender = '';
+    for (i of t) {
+        if ( i.checked == true) {
+            gender = i.value;
+        }
+    }
+
+    let row = document.createElement('tr');
+    row.setAttribute('scope','row');
+
+    let c1 = document.createElement('td');
+    let c2 = document.createElement('td');
+    let c3 = document.createElement('td');
+    let c4 = document.createElement('td');
+    let c5 = document.createElement('td');
+
+    c1.innerHTML = firstname;
+    c2.innerHTML = lastname;
+    c3.innerHTML = email;
+    c4.innerHTML=phone;
+    c5.innerHTML=gender;
+
+    row.appendChild(c1);
+    row.appendChild(c2);
+    row.appendChild(c3);
+    row.appendChild(c4);
+    row.appendChild(c5);
+
+    document.getElementsByTagName('tbody')[0].appendChild(row);
+
+    clearForm();
+}
+
+function clearForm(){
+    document.getElementById('firstname').value = '';
+    document.getElementById('lastname').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+}
+
+function getrndmnumberusers(){
+    function getandsubmit(xhr){
+        getrndmuser(xhr);
+        submittotable();
+    }
+
+    let n = document.getElementById('nusers').value;
+    n = Number(n);
+    for ( let i = 0; i < n; i++ ) {
+        sendAjaxGet(url,getandsubmit);
+    }
+}
