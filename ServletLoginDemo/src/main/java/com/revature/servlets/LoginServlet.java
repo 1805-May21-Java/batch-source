8562634fd96d6374617a0b0ff51e4426367cd86e
Carolyn Rehm <carolynhrehm@gmail.com>
2018-06-12 16:02:06 -0400

    servlet demos

---
+++ ServletLoginDemo/src/main/java/com/revature/servlets/LoginServlet.java
@@ -0,0 +1,37 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		HttpSession session = request.getSession();
+		session.invalidate();
+		request.getRequestDispatcher("Login.html").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String user = request.getParameter("username");
+		String pass = request.getParameter("password");
+		
+		HttpSession session = request.getSession();
+		
+		if(user.equals("admin") && pass.equals("pass123")) {
+			//System.out.println("login was successful - correct credentials");
+			session.setAttribute("username", "admin");
+			response.sendRedirect("profile");
+		} else {
+			//System.out.println("login was not successful - please try again");
+			response.sendRedirect("login");
+		}
+	}
+
+}
