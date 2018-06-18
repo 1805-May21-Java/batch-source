9a09b0bb57231ee6fb1a6dd5d5254815862fc403
Sydney Mercier <sydney.mercier91@gmail.com>
2018-06-18 15:26:48 -0400

    hide profile on button click

--- Angular Projects/angular-practice/src/app/profile/profile.component.ts
+++ Angular Projects/angular-practice/src/app/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../user';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  buttonLabel = "Hide";
+  toggle = true;
+
+  user: User = {
+    id: 1,
+    name: 'Sydney Mercier',
+    dob: 'February 24, 1991'
+  };
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  hideProfile(): void {
+    this.toggle = !this.toggle;
+    if (this.buttonLabel == "Hide") {
+      this.buttonLabel = "Show";
+    } else {
+      this.buttonLabel = "Hide";
+    }
   }
 
 }
